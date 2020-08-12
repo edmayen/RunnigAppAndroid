113055d057280a21b9012730d568707eaecd0b2a
edmayen <ed_mayen@hotmail.com>
2020-08-12 16:24:38 -0500

    Updating the Notification

--- app/src/main/java/com/dev/runningapp/other/TrackingUtility.kt
+++ app/src/main/java/com/dev/runningapp/other/TrackingUtility.kt
@@ -34,8 +34,8 @@ object TrackingUtility {
         val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds)
         if (!includeMillis) {
             return "${if (hours < 10) "0" else ""}$hours" +
-                    "${if (minutes < 10) "0" else ""}$minutes" +
-                    "${if (seconds < 10) "0" else ""}$seconds"
+                    ":${if (minutes < 10) "0" else ""}$minutes" +
+                    ":${if (seconds < 10) "0" else ""}$seconds"
         }
         milliseconds -= TimeUnit.SECONDS.toMillis(seconds)
         milliseconds /= 10
