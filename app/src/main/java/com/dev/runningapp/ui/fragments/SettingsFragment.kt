5eb981a5fc0ce36bd66943ea68866a7bec9233d0
edmayen <ed_mayen@hotmail.com>
2020-12-26 18:11:25 -0600

    Total run statistics and SettingsFragment

--- app/src/main/java/com/dev/runningapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/dev/runningapp/ui/fragments/SettingsFragment.kt
@@ -1,7 +1,59 @@
 package com.dev.runningapp.ui.fragments
 
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.dev.runningapp.R
+import com.dev.runningapp.other.Constants.KEY_NAME
+import com.dev.runningapp.other.Constants.KEY_WEIGHT
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_settings.*
+import kotlinx.android.synthetic.main.fragment_setup.*
+import kotlinx.android.synthetic.main.fragment_setup.etName
+import kotlinx.android.synthetic.main.fragment_setup.etWeight
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
+
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadFieldsFromSharedPref()
+        btnApplyChanges.setOnClickListener {
+            val success = applyChangesToSharedPref()
+            if (success){
+                Snackbar.make(view, "Saved changes", Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(view, "PLease fill out all the fields", Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun loadFieldsFromSharedPref(){
+        val name = sharedPref.getString(KEY_NAME, "")
+        val weight = sharedPref.getFloat(KEY_WEIGHT, 80f)
+        etName.setText(name)
+        etWeight.setText(weight.toString())
+    }
+
+    private fun applyChangesToSharedPref(): Boolean{
+        val nameText = etName.text.toString()
+        val weightText = etWeight.text.toString()
+        if (nameText.isEmpty() || weightText.isEmpty()){
+            false
+        }
+        sharedPref.edit()
+            .putString(KEY_NAME, nameText)
+            .putFloat(KEY_WEIGHT, weightText.toFloat())
+            .apply()
+        val toolbarText = "Let's go $nameText"
+        requireActivity().tvToolbarTitle.text = toolbarText
+        return true
+    }
 }
--- app/src/main/java/com/dev/runningapp/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/dev/runningapp/ui/viewmodels/StatisticsViewModel.kt
@@ -9,4 +9,11 @@ import com.dev.runningapp.repositories.MainRepository
 class StatisticsViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+
+    val totalTimeRun = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+    val totalAvgSpeed = mainRepository.getTotalAvgSpeed()
+
+    val runSortedByDate = mainRepository.getAllRunsSortedByDate()
 }
